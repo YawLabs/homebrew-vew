5196fc09c5bd07fd45cbc8df0a0abf101fd2f05d
Jeff Hale <jeff@yaw.sh>
2026-06-22 18:54:30 -0700

    Bump to v0.6.2

--- Casks/vew.rb
+++ Casks/vew.rb
@@ -1,11 +1,11 @@
 cask "vew" do
   arch arm: "arm64", intel: "x64"
-  version "0.6.1"
+  version "0.6.2"
 
   if Hardware::CPU.arm?
-    sha256 "f2b0054f0ef2f35071f92e0a2b9e2cfdec71dcd9252fd88a0316d1b38860cd46"
+    sha256 "d3282cc4493ba0a7a5108d2d7d997b7b83410aa04a5595abce453ae64c430493"
   else
-    sha256 "beb33b74497740b87658410cbca42be1392b363148c0ae914c84d6e3cec50731"
+    sha256 "adc391e6403b04a5136cf26a18c6786cab70c96795cf2f50bcd77edfb8990778"
   end
 
   url "https://downloads.vew.sh/vew-darwin-#{arch}-#{version}.zip"
